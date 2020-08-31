264e07575d42e50bac225d671b38896a6a37135c
Matt Parker <mattmparker@gmail.com>
2020-08-31 09:54:02 -0600

    Check machine_info for all files

--- try_this.R
+++ try_this.R
@@ -1,41 +1,31 @@
-
 library(dplyr)
 library(rvest)
 
 
-input_directory <- 'r:/shared documents/'
-output_directory <- getwd()
-
-
-files_to_process <- list.files(, 
+files_to_process <- list.files('r:/shared documents/', 
                                pattern = 'html', 
                                full.names = TRUE)
 
 
-# Check that these are the right file paths
-files_to_process
 
-this_file <- files_to_process[1]
-
-
-# Delete the "Software Inventory" multi-column header
-this_file_parsed <- readLines(this_file) %>% 
-    .[!grepl(x = ., pattern = "Software Inventory")] %>%
-    paste(collapse = "") %>%
-    read_html()
-
-
-machine_info <- this_file_parsed %>% 
-    html_node("#machineInfo") %>% 
-    html_table(header = FALSE)
-
-
-
-# Does this print just the Workstation Name?
-machine_info[ grepl(x = machine_info[ , 1], pattern = 'Workstation Name'), 2]
-                                 
-
-machine_info[ grepl(x = machine_info[ , 1], pattern = 'Logon'), 2]
-
-
+software_inventory_all <- lapply(files_to_process, FUN = function(this_file) {
+    
+    print(paste("Extracting data from", this_file))
+    
+    # Delete the "Software Inventory" multi-column header
+    this_file_parsed <- readLines(this_file) %>% 
+        .[!grepl(x = ., pattern = "Software Inventory")] %>%
+        paste(collapse = "") %>%
+        read_html()
+    
+    # Print each 
+    machine_info <- this_file_parsed %>% 
+        html_node("#machineInfo") %>% 
+        html_table(header = TRUE)
+  
+    print(paste0("Machine info table from file ", this_file)) 
+  
+    print(machine_info)
+    
+})
 
